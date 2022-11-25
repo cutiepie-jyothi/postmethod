dba90f644311a83dd6eba21378584bdaf7df0750
Tejasri-99 <tejapamulapati99@gmail.com>
2022-11-25 13:45:09 +0530

    post

---
+++ index.js
@@ -0,0 +1,25 @@
+const express=require("express")
+const app=express()
+const cors=require("cors")
+const bodyparser=require("body-parser")
+const port=3078
+
+app.use(bodyparser.urlencoded({
+        extended:true
+    }))
+app.use(bodyparser.json())
+
+app.use(cors())
+
+app.get("/",(req,res)=>{
+    res.send("server is starting")
+})
+app.post("/newData",(req,res)=>{
+   const {name,age,password}=req.body
+   console.log(name,age,password)
+   res.send("added successfully")
+})
+
+app.listen(port,()=>console.log("server is running on port 3078"))
+
+
